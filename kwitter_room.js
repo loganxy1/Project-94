2f62fe5702f44510951ebb0eb1e39ba9a2b2c57c
loganxy1 <80921776+loganxy1@users.noreply.github.com>
2021-08-12 13:52:19 -0400

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyCRDp0g5KLMcQ4vmtTjTkBshb6FMpZJGxU",
+    authDomain: "project-94-e487a.firebaseapp.com",
+    projectId: "project-94-e487a",
+    storageBucket: "project-94-e487a.appspot.com",
+    messagingSenderId: "977296758392",
+    appId: "1:977296758392:web:5137dad11885430cecff8d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
